1dbcec823651291a09826a216033e0716ea08523
Alexandra Prodan <prodan.sasha@gmail.com>
2015-12-08 21:40:27 -0600

    Chapter 2: Variables

---
+++ Variables/name.rb
@@ -0,0 +1,5 @@
+
+
+puts "Type in your name, please!"
+name = gets.chomp
+puts "How are you doing, " + name + "!"
---
+++ Variables/name_10.rb
@@ -0,0 +1,7 @@
+
+
+puts "Type in your name, please!"
+name = gets.chomp
+10.times do |n|
+    puts name
+end
